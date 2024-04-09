2cd62a3613742618ca9ea99018d755bc47a3017e
Febriyadi <165775349+EtxFebriyadi@users.noreply.github.com>
2024-04-09 05:42:56 +0000

    Add Array in structure

--- .ccls-cache/@home@runner@structure-Data/Structure@struct.cpp
+++ .ccls-cache/@home@runner@structure-Data/Structure@struct.cpp
@@ -5,45 +5,48 @@ struct Mahasiswa {
   string nim, nama, jurusan;
   int umur;
   string hobi[3];
-} mhs1, mhs2;
+} mhs1, mhs[1];
 int main() {
   string hobiMahasiswa[3] = {"Berenang", "Membaca", "Menulis"};
+
+  //Array dalam structure
+  Mahasiswa mhs[3];
   // cara 1
-  mhs1.nim = "F55121082";
-  mhs1.nama = "Febriyadi";
-  mhs1.jurusan = "Teknik Informatika";
-  mhs1.umur = 20;
+  mhs[0].nim = "F55121082";
+  mhs[0].nama = "Febriyadi";
+  mhs[0].jurusan = "Teknik Informatika";
+  mhs[0].umur = 20;
 
   for (int i = 0; i < 3; ++i) {
-    mhs1.hobi[i] = hobiMahasiswa[i];
+    mhs[0].hobi[i] = hobiMahasiswa[i];
   }
 
-  cout << "NPM Mahasiswa 1 :" << mhs1.nim << endl;
-  cout << "Nama Mahasiswa 1 : " << mhs1.nama << endl;
-  cout << "Jurusan Mahasiswa 1 : " << mhs1.jurusan << endl;
-  cout << "Umur Mahasiswa 1 : " << mhs1.umur << endl;
-  cout << "Hobi Mahasiswa 1 : " << mhs1.hobi[0] << endl;
+  cout << "NPM Mahasiswa 1 :" << mhs[0].nim << endl;
+  cout << "Nama Mahasiswa 1 : " << mhs[0].nama << endl;
+  cout << "Jurusan Mahasiswa 1 : " << mhs[0].jurusan << endl;
+  cout << "Umur Mahasiswa 1 : " << mhs[0].umur << endl;
+  cout << "Hobi Mahasiswa 1 : " << mhs[0].hobi[0] << endl;
   // cara 2
-  mhs2 = {"F55121077",
+  mhs[1] = {"F55121077",
           "Yunita",
           "Teknik Informatika",
           21,
           {"Mancing", "Ngoding", "Nonton"}};
-  cout << "\nNPM Mahasiswa 2:" << mhs2.nim << endl;
-  cout << "Nama Mahasiswa 2 : " << mhs2.nama << endl;
-  cout << "Jurusan Mahasiswa 2 : " << mhs2.jurusan << endl;
-  cout << "Umur Mahasiswa 2: " << mhs2.umur << endl;
-  cout << "Hobi Mahasiswa 1 : " << mhs2.hobi[1] << endl;
+  cout << "\nNPM Mahasiswa 2:" << mhs[1].nim << endl;
+  cout << "Nama Mahasiswa 2 : " << mhs[1].nama << endl;
+  cout << "Jurusan Mahasiswa 2 : " << mhs[1].jurusan << endl;
+  cout << "Umur Mahasiswa 2: " << mhs[1].umur << endl;
+  cout << "Hobi Mahasiswa 1 : " << mhs[1].hobi[1] << endl;
 
   // Cara 3
-  Mahasiswa mhs3 = {"F55121077",
+  mhs[2] = {"F55121077",
                     "Anggeraini",
                     "Teknik Informatika",
                     21,
                     {"Mancing", "Ngoding", "Nonton"}};
-  cout << "\nNPM Mahasiswa 2:" << mhs3.nim << endl;
-  cout << "Nama Mahasiswa 2 : " << mhs3.nama << endl;
-  cout << "Jurusan Mahasiswa 2 : " << mhs3.jurusan << endl;
-  cout << "Umur Mahasiswa 2: " << mhs3.umur << endl;
-  cout << "Hobi Mahasiswa 1 : " << mhs3.hobi[1] << endl;
+  cout << "\nNPM Mahasiswa 2:" << mhs[2].nim << endl;
+  cout << "Nama Mahasiswa 2 : " << mhs[2].nama << endl;
+  cout << "Jurusan Mahasiswa 2 : " << mhs[2].jurusan << endl;
+  cout << "Umur Mahasiswa 2: " << mhs[2].umur << endl;
+  cout << "Hobi Mahasiswa 1 : " << mhs[2].hobi[1] << endl;
 }
---
+++ Array/Readme.md
@@ -0,0 +1,3 @@
+1. Arrya dari Structure
+2. Nested Structure
+   - Bisa dilakukan dalam 2 cara yaitu :
